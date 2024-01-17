a87e6aba4fd921d10e0267033cc631641cde8997
paul77uk <paul77uk@gmail.com>
2024-01-17 13:44:30 +0000

    removed force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import prisma from "@/lib/prisma";
 import Link from "next/link";
 
-export const dynamic = "force-dynamic";
-
 export default async function Home() {
   const programs = await prisma.program.findMany();
 
