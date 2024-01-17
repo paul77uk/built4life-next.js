7fd235278287a71a0f3d4e0af2ee6e1ce4eedf7c
paul77uk <paul77uk@gmail.com>
2024-01-17 01:12:44 +0000

    used prisma findmany() to get programs from db and show to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <p className="font-semibold">Hello Built4Life!</p>;
+import prisma from "@/lib/prisma";
+
+export default async function Home() {
+  const programs = await prisma.program.findMany();
+
+  const renderedPrograms = programs.map((program) => (
+    <div key={program.id}>{program.name}</div>
+  ));
+
+  return renderedPrograms
 }
